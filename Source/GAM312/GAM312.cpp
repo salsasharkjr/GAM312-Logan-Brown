d58d128f674cce7e206390fc58a01d001486431e
Logan <logan.brown5@snhu.edu>
2025-01-12 23:11:43 -0500

    Initial Commit

---
+++ Source/GAM312/GAM312.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312, "GAM312" );
